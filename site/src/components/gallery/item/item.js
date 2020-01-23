dd37059fd73b0bcd614d2babb9301baa4b5ea069
eisenivan <ivan.eisenberg@gmail.com>
2020-01-23 15:31:04 -0600

    add spotify url to list

--- site/src/components/gallery/item/item.js
+++ site/src/components/gallery/item/item.js
@@ -3,14 +3,18 @@ import PropTypes from 'prop-types'
 import get from 'lodash.get'
 import { Title, Image, Tags, Tag } from './item.css'
 
-const Item = ({ name, images, genres }) => (
+const Item = ({ name, images, genres, external_urls: externalUrls }) => (
   <figure>
     { get(images, '[0].url')
       ? <Image src={get(images, '[0].url', '')} alt={name} />
       : <Image src='https://picsum.photos/640/640/?blur=2' alt={name} /> }
 
     <figcaption>
-      <Title>{name}</Title>
+      <Title>
+        <a target='blank' href={externalUrls.spotify}>
+          {name}
+        </a>
+      </Title>
       <Tags>{genres.map(x => <Tag key={`${name}-${x}`}>{x}</Tag>)}</Tags>
     </figcaption>
   </figure>
@@ -19,7 +23,8 @@ const Item = ({ name, images, genres }) => (
 Item.propTypes = {
   name: PropTypes.string,
   images: PropTypes.arrayOf(PropTypes.object),
-  genres: PropTypes.array
+  genres: PropTypes.array,
+  external_urls: PropTypes.object
 }
 
 export default Item
