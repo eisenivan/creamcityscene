483a5eac1cd63b2008afb40c1b1d7356d5276655
Ivan Eisenberg <ivaneisenberg@northwesternmutual.com>
2021-10-27 13:57:53 -0500

    security patches

--- cache-artists.js
+++ cache-artists.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const axios = require('axios')
 const fs = require('fs')
 const uniq = require('lodash.uniq')
@@ -8,7 +9,7 @@ const moment = require('moment')
 const today = moment().format('YYYY-MM-DD')
 const thisWeek = moment().add(7, 'days').format('YYYY-MM-DD')
 
-axios.get(`https://api.songkick.com/api/3.0/metro_areas/15405-us-milwaukee/calendar.json?apikey=lUMYaWAtyzEue1rK&min_date=${today}&max_date=${thisWeek}`)
+axios.get(`https://api.songkick.com/api/3.0/metro_areas/15405-us-milwaukee/calendar.json?apikey=${process.env.apikey}&min_date=${today}&max_date=${thisWeek}`)
   .then(({ data }) => {
     fs.writeFileSync('./site/content/shows/shows.json', JSON.stringify(data.resultsPage.results.event))
     const reduced = uniq(flatten(get(data, 'resultsPage.results.event', []).map(x => x.performance.map(y => y.artist.displayName))))
